a9e82a06e72c0c9273e9b66ce791f3a7812aa11c
asuen39 <aiegdnmissi.2405@gmail.com>
2023-07-17 05:09:23 +0900

    nunjucks filterの処理追加。 追加にかなり時間が掛かってしまった。
    理由: バージョンによって記述方が変わっており、参考にしたサイト等は古いやり方の為変数が認識できないとエラーが出てしまっていた。

--- app.js
+++ app.js
@@ -4,25 +4,32 @@ const express = require('express');
 var app = express();
 const port = 8000;
 
-nunjucks.configure('.', {
+const env = nunjucks.configure('.', {
     autoescape: true,
     express: app
 });
 
+// カスタムフィルターの定義
+env.addFilter('uppercase', function(str) {
+    return str.toUpperCase();
+});
+
 app.get('', (req, res) => {
 
-    //1. 設定された変数に対してメッセージを送る。
-    //res.render('index.html', { message: 'nunjucksテンプレートがローカルで起動出来た。' });
+  //1. 設定された変数に対してメッセージを送る。
+  //res.render('index.html', { message: 'nunjucksテンプレートがローカルで起動出来た。' });
 
-    //2. オブジェクトのプロパティへのアクセ
-    const person = {
-        message: 'nunjucksテンプレートがローカルで起動出来た。',
-        name: 'John',
-        age: 30
-    };
+  //2. オブジェクトのプロパティへのアクセ
+  const person = {
+    message: 'nunjucksテンプレートがローカルで起動出来た。',
+    name: 'John',
+    age: 30
+  };
 
-    // テンプレートにデータを渡す
-    res.render('index.html', { person });
+  const message = 'Hello, World!';
+
+  // テンプレートにデータを渡す
+  res.render('index.html', { message, person });
 });
 
 app.listen(port, () => {
