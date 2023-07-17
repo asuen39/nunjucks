510b64495aab9ba380751ec725e4853666789438
asuen39 <aiegdnmissi.2405@gmail.com>
2023-07-18 02:15:46 +0900

    テンプレートタグの習得

--- app.js
+++ app.js
@@ -10,11 +10,19 @@ const env = nunjucks.configure('.', {
 });
 
 // カスタムフィルターの定義
-env.addFilter('uppercase', function(str) {
-    return str.toUpperCase();
+env.addFilter('uppercase', function(value) {
+    return value.toUpperCase();
 });
 
-app.get('', (req, res) => {
+env.addFilter('reverse', (value) => {
+  return value.split('').reverse().join('');
+});
+
+env.addFilter('int', (value) => {
+  return parseInt(value, 10);
+});
+
+app.get('', async (req, res) => {
 
   //1. 設定された変数に対してメッセージを送る。
   //res.render('index.html', { message: 'nunjucksテンプレートがローカルで起動出来た。' });
@@ -26,10 +34,16 @@ app.get('', (req, res) => {
     age: 30
   };
 
-  const message = 'Hello, World!';
+  const message = 'フィルターメッセージ';
+  const age2 = '30';
 
   // テンプレートにデータを渡す
-  res.render('index.html', { message, person });
+  res.render('index.html', { message, person, age2 });
+});
+
+//page2.htmlにテンプレートデータを渡す。
+app.get('/page2', (req, res) => {
+  res.render('page2.html');
 });
 
 app.listen(port, () => {
